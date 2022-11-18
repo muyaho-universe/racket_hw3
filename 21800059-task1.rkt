d730e3b86eea397310b0d79d073729947dfee704
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2022-11-18 22:38:00 +0900

    no typo

--- 21800059-task1.rkt
+++ 21800059-task1.rkt
@@ -16,28 +16,12 @@
   [fun(param symbol?)(body-expr LFAE?)]
   [app (f LFAE?) (a LFAE?)])
 
-; num-op: (number number -> number) -> (FWAE FWAE -> FWAE)
+; num-op: (number number -> number) -> (LFAE LFAE -> LFAE)
 (define (num-op op)
      (lambda (x y)
           (num (op (num-n x) (num-n y)))))
 (define num+ (num-op +))
 (define num- (num-op -))
-
-
-;Implementing LFAE
-(define (interp lfae ds)
-  (type-case LFAE lfae
-    [num (n) (numV n)]
-    [add (l r) (num+ (interp l ds) (interp r ds))]
-    [sub (l r) (num- (interp l ds) (interp r ds))]
-    [id (name) (lookup name ds)]
-    [fun (param body-expr) (closureV param body-expr ds)]
-    [app (f a) (local[(define ftn-v (strict(interp f ds (box #f))))
-                      (difine arg-v (exprV a ds))]
-                 (interp (closureV-body ftn-v)
-                         (aSub (closureV-param ftn-v)
-                               arg-v
-                               (clousreV-ds ftn-v))))]))
 
 ;Implementing LFAE: LFAE Values
 (define-type LFAE-Value
@@ -48,6 +32,19 @@
   [exprV (expr LFAE?) (ds DefrdSub?)
          (value (box/c (or/c false LFAE-Value?)))])
 
+;DefrdSub
+(define-type DefrdSub
+    [mtSub]
+    [aSub (name symbol?) (value LFAE-Value?) (ds DefrdSub?)])
+
+;look up
+(define (lookup name ds)
+  (cond
+    [(empty? ds) (error 'lookup "name not found")]
+    [else (cond
+            [(symbol=? (name (first ds)))
+             name]
+            [else (lookup name (rest ds))])]))
 ;strict: LFAE-Value -> LFAE-Value
 (define (strict v)
   (type-case LFAE-Value v
@@ -58,3 +55,18 @@
                         v))
                (unbox v-box))]
     [else v]))
+
+;Implementing LFAE
+(define (interp lfae ds)
+  (type-case LFAE lfae
+    [num (n) (numV n)]
+    [add (l r) (num+ (interp l ds) (interp r ds))]
+    [sub (l r) (num- (interp l ds) (interp r ds))]
+    [id (name) (lookup name ds)]
+    [fun (param body-expr) (closureV param body-expr ds)]
+    [app (f a) (local[(define ftn-v (strict(interp f ds (box #f))))
+                      (define arg-v (exprV a ds))]
+                 (interp (closureV-body ftn-v)
+                         (aSub (closureV-param ftn-v)
+                               arg-v
+                               (closureV-ds ftn-v))))]))
